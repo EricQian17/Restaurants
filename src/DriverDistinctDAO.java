484600b5c834fe4701e12399199ac516f29b55e0
Eric Qian <42530687+EricQian17@users.noreply.github.com>
2021-04-14 14:47:26 -0400

    Update DriverDistinctDAO.java

--- src/DriverDistinctDAO.java
+++ src/DriverDistinctDAO.java
@@ -8,7 +8,7 @@ public class DriverDistinctDAO {
 	public static void main(String[] args) {
 		
 		Map<String, String> mapDB = new HashMap<>();
-		mapDB.put("uri", "mongodb://eqian17:EEvee1337@cluster0-shard-00-00.1jdg9.mongodb.net:27017,cluster0-shard-00-01.1jdg9.mongodb.net:27017,cluster0-shard-00-02.1jdg9.mongodb.net:27017/sample_restaurants?ssl=true&replicaSet=atlas-9ozhs4-shard-0&authSource=admin&retryWrites=true&w=majority");
+		mapDB.put("uri", "mongodb://eqian17:password1029@cluster0-shard-00-00.1jdg9.mongodb.net:27017,cluster0-shard-00-01.1jdg9.mongodb.net:27017,cluster0-shard-00-02.1jdg9.mongodb.net:27017/sample_restaurants?ssl=true&replicaSet=atlas-9ozhs4-shard-0&authSource=admin&retryWrites=true&w=majority");
 		mapDB.put("db", "sample_restaurants");
 		mapDB.put("col", "restaurants");
 		
